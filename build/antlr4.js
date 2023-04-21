e9123c5cab4527e905337174427657ac6ac10f74
wewoor <wewoor@gmail.com>
2023-04-21 16:50:36 +0800

    build: update the compiling logic for antlr4

--- build/antlr4.js
+++ build/antlr4.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const exec = require('child_process').exec;
+const argv = require('yargs-parser')(process.argv.slice(2))
 
 const antlr4 = path.resolve(__dirname, './antlr-4.12.0-complete.jar');
 const grammars = path.resolve(__dirname, '../src/grammar');
@@ -14,7 +15,7 @@ const entry = [
     'flinksql',
 ];
 
-entry.forEach((language) => {
+function compile(language) {
     const cmd = `
         java -jar ${antlr4}
         -Dlanguage=TypeScript 
@@ -23,7 +24,7 @@ entry.forEach((language) => {
         -o ${output}/${language}
         ${grammars}/${language}/*.g4 
     `.replace(/\n/g, '');
-    console.log('cmd:', cmd);
+    console.info('Executing:', cmd);
     exec(cmd, (err) => {
         if (err) {
             console.error('Antlr4 build error: ' + language, err);
@@ -31,5 +32,20 @@ entry.forEach((language) => {
             console.log(`Build ${language} success.`);
         }
     });
-});
+}
+
+if (argv.all) { // build all: yarn antlr4 --all
+    entry.forEach((language) => {
+        compile(language);
+    });
+} else if (argv.lang) {// build single: yarn antlr4 --lang=generic
+    const supportedLanguage = entry.find((language) => language === argv.lang);
+    if (supportedLanguage) {
+        compile(argv.lang);
+    } else {
+        console.error('Unsupported language: ' + argv.lang);
+    }
+} else {
+    console.error('Please to specify the language, just like: yarn antlr4 --lang flinksql');
+}
 
