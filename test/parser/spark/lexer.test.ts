da621eec33820a90fd3661056adfd4ea5f082742
wewoor <wewoor@gmail.com>
2023-04-24 11:15:35 +0800

    fix: typing errors

--- test/parser/spark/lexer.test.ts
+++ test/parser/spark/lexer.test.ts
@@ -1,14 +1,11 @@
 import SparkSQL from '../../../src/parser/spark';
 
-const log = console.log.bind(console);
-
 describe('SparkSQL Lexer tests', () => {
     const parser = new SparkSQL();
 
     test('select id,name from user1;', () => {
         const sql = `select id,name from user1;`;
         const tokens = parser.getAllTokens(sql);
-        const length = tokens.length;
         expect(tokens.length - 1).toBe(10);
     });
 
