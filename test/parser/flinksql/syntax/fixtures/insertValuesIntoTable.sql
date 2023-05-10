5b580ca121205393b288f10d9fdb3b0e0236095b
mumiao <1270865802zl@gmail.com>
2023-05-10 16:07:53 +0800

    feat: complete insertValuesIntoTable syntax unit tests

--- test/parser/flinksql/syntax/fixtures/insertValuesIntoTable.sql
+++ test/parser/flinksql/syntax/fixtures/insertValuesIntoTable.sql
@@ -1,3 +1,22 @@
-INSERT INTO students
-VALUES ('fred flintstone', 35, 1.28),
+INSERT INTO country_page_view
+VALUES ('Chinese', 'mumiao', 18),
+    ('barney rubble', 32, 2.32);
+
+EXECUTE 
+INSERT INTO country_page_view
+VALUES ('Chinese', 'mumiao', 18),
+    ('barney rubble', 32, 2.32);
+
+EXECUTE
+INSERT OverWrite country_page_view
+VALUES ('Chinese', 'mumiao', 18),
+    ('barney rubble', 32, 2.32);
+
+EXECUTE
+INSERT INTO country_page_view
+VALUES ('Chinese', 'mumiao', 18),
+    ('barney rubble', 32, 2.32);
+
+INSERT INTO catalog1.db1.country_page_view
+VALUES ('Chinese', 'mumiao', 18),
     ('barney rubble', 32, 2.32);
