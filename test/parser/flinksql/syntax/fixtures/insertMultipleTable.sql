a63e3c4d5166dcb278c515a6e5781769975faef0
mumiao <1270865802zl@gmail.com>
2023-05-11 20:09:12 +0800

    test: fix insertMultipleTable sql error

--- test/parser/flinksql/syntax/fixtures/insertMultipleTable.sql
+++ test/parser/flinksql/syntax/fixtures/insertMultipleTable.sql
@@ -1,7 +1,9 @@
 EXECUTE STATEMENT SET 
 BEGIN
+INSERT INTO country_page_view
 VALUES ('Chinese', 'mumiao', 18),
     ('Amercian', 'georage', 22);
+INSERT INTO country_page_view
 VALUES ('Chinese', 'mumiao', 18),
     ('Amercian', 'georage', 22);
 END;
