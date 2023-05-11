5173ac9eeee9adcb737297567e583fa5a794c866
mumiao <1270865802zl@gmail.com>
2023-05-11 11:46:32 +0800

    feat:  improve  insertMultipleTable.sql  test case

--- test/parser/flinksql/syntax/fixtures/insertMultipleTable.sql
+++ test/parser/flinksql/syntax/fixtures/insertMultipleTable.sql
@@ -1,11 +1,9 @@
 EXECUTE STATEMENT SET 
 BEGIN
-INSERT INTO students
-VALUES ('fred flintstone', 35, 1.28),
-    ('barney rubble', 32, 2.32);
-INSERT INTO students
-VALUES ('fred flintstone', 35, 1.28),
-    ('barney rubble', 32, 2.32);
+VALUES ('Chinese', 'mumiao', 18),
+    ('Amercian', 'georage', 22);
+VALUES ('Chinese', 'mumiao', 18),
+    ('Amercian', 'georage', 22);
 END;
 
 
