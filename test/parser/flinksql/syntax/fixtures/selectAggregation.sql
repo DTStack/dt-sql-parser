61a884ba8c55b0c9908b6f0445fcddfb979f99f5
hayden <hayden9653@gmail.com>
2023-05-16 11:55:33 +0800

    test: add over aggregation grammar test cases

--- test/parser/flinksql/syntax/fixtures/selectAggregation.sql
+++ test/parser/flinksql/syntax/fixtures/selectAggregation.sql
@@ -66,31 +66,57 @@ GROUP BY
 
 -- Group Window Aggregation
 SELECT
-  user,
-  TUMBLE_START(order_time, INTERVAL '1' DAY) AS wStart,
-  SUM(amount) FROM Orders
+    user,
+    TUMBLE_START(order_time, INTERVAL '1' DAY) AS wStart,
+    SUM(amount) FROM Orders
 GROUP BY
-  TUMBLE(order_time, INTERVAL '1' DAY),
-  user;
+    TUMBLE(order_time, INTERVAL '1' DAY),
+    user;
 
 SELECT
-  user,
-  TUMBLE_START(order_time, INTERVAL '1' DAY) AS wStart,
-  SUM(amount) FROM Orders
+    user,
+    TUMBLE_START(order_time, INTERVAL '1' DAY) AS wStart,
+    SUM(amount) FROM Orders
 GROUP BY
-  HOP(order_time, INTERVAL '1' DAY),
-  user;
+    HOP(order_time, INTERVAL '1' DAY),
+    user;
 
 SELECT
-  user,
-  TUMBLE_START(order_time, INTERVAL '1' DAY) AS wStart,
-  SUM(amount) FROM Orders
+    user,
+    TUMBLE_START(order_time, INTERVAL '1' DAY) AS wStart,
+    SUM(amount) FROM Orders
 GROUP BY
-  SESSION(order_time, INTERVAL '1' DAY),
-  user;
+    SESSION(order_time, INTERVAL '1' DAY),
+    user;
 
 -- Having
 SELECT SUM(amount)
 FROM Orders
 GROUP BY users
 HAVING SUM(amount) > 50;
+
+-- Over Aggregation
+SELECT order_id, order_time, amount,
+    SUM(amount) OVER (
+        PARTITION BY product
+        ORDER BY order_time
+        RANGE BETWEEN INTERVAL '1' HOUR PRECEDING AND CURRENT ROW
+    ) AS one_hour_prod_amount_sum
+FROM Orders;
+
+SELECT product, order_time, amount,
+    SUM(amount) OVER (
+        PARTITION BY product
+        ORDER BY order_time
+        ROWS BETWEEN 5 PRECEDING AND CURRENT ROW
+    ) AS one_hour_prod_amount_sum
+FROM source_table;
+
+SELECT order_id, order_time, amount,
+    SUM(amount) OVER w AS sum_amount,
+    AVG(amount) OVER w AS avg_amount
+FROM Orders
+WINDOW w AS (
+    PARTITION BY product
+    ORDER BY order_time
+    RANGE BETWEEN INTERVAL '1' HOUR PRECEDING AND CURRENT ROW);
