38ad5dc7f215e38a78c8c086ffe538bad4bdb62f
mumiao <1270865802zl@gmail.com>
2023-05-23 16:45:54 +0800

    test(trino): improve update statement test cases

--- test/parser/trinosql/syntax/fixtures/update.sql
+++ test/parser/trinosql/syntax/fixtures/update.sql
@@ -1,1 +1,10 @@
-UPDATE foo_tablen    SET bar = 23, baz = 3.1415E0, bletch = 'barf' WHERE (nothing = 'fun');
+UPDATE foo_tablen  SET bar = 23, baz = 3.1415E0, bletch = 'barf' WHERE (nothing = 'fun');
+
+UPDATE new_hires SET manager = (
+    SELECT
+      e.name
+    FROM
+      employees e
+    WHERE
+      e.employee_id = new_hires.manager_id
+);
