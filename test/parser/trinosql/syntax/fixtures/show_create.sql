75570198db9049fe037d357aa70600211428f08a
mumiao <1270865802zl@gmail.com>
2023-05-23 16:28:02 +0800

    test(trino): complete show statement unit case

---
+++ test/parser/trinosql/syntax/fixtures/show_create.sql
@@ -0,0 +1,16 @@
+SHOW CREATE TABLE sf1.orders;
+
+SHOW CREATE SCHEMA IF NOT EXISTS traffic;
+
+SHOW CREATE VIEW test AS
+SELECT orderkey, orderstatus, totalprice / 2 AS half
+FROM orders;
+
+SHOW CREATE MATERIALIZED VIEW cancelled_orders
+AS
+    SELECT orderkey, totalprice
+    FROM orders
+    WHERE orderstatus = 3;
+
+    
+
