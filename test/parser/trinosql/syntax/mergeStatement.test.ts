f1267bf35801ffcd12f32704b7f9f87a5a661d98
mumiao <1270865802zl@gmail.com>
2023-05-24 11:50:00 +0800

    test(trino): complete merge and reset session statement unit cases

---
+++ test/parser/trinosql/syntax/mergeStatement.test.ts
@@ -0,0 +1,17 @@
+import TrinoSQL from "../../../../src/parser/trinosql";
+import { readSQL } from "../../../helper";
+
+const features = {
+    merge: readSQL(__dirname, 'merge.sql'),
+};
+
+describe('TrinoSQL Merge Statements Syntax Tests', () => {
+    const parser = new TrinoSQL();
+    // merge statements
+    features.merge.forEach((sql) => {
+        it(sql, () => {
+            expect(parser.validate(sql).length).toBe(0);
+        });
+    });
+});
+
