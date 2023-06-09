97b413d65ed122970c2e6b5f32b44199f753e3c2
hayden <hayden9653@gmail.com>
2023-06-09 16:16:03 +0800

    test: add multiple sql test case of suggestion

---
+++ test/parser/flinksql/suggestion/fixtures/multipleSql.sql
@@ -0,0 +1,19 @@
+CREATE TABLE orders (
+    order_uid  BIGINT,
+    product_id BIGINT,
+    price      DECIMAL(32, 2),
+    order_time TIMESTAMP(3)
+) WITH (
+    'connector' = 'datagen'
+);
+
+CREATE TABLE orders (
+    order_uid  BIGINT,
+    product_id BIGINT,
+    price      DECIMAL(32, 2),
+    order_time TIMESTAMP(3)
+) WITH (
+    'connector' = 'datagen'
+);
+
+use cat1.
