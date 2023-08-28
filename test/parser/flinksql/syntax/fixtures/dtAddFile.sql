9bd842b7d6c52c455ab1f5f03c9ad57c8e993ea5
hayden <hayden9653@gmail.com>
2023-08-28 13:05:31 +0800

    test: patch dtAddFile syntax unit tests

--- test/parser/flinksql/syntax/fixtures/dtAddFile.sql
+++ test/parser/flinksql/syntax/fixtures/dtAddFile.sql
@@ -2,6 +2,8 @@ ADD JAR WITH /home/test.jar;
 
 ADD JAR WITH /home/test.jar AS test.jar;
 
+ADD FILE WITH /home/admin/sftp/dttest_cn/DsCenter_1687/krb5.conf;
+
 ADD FILE WITH /home/admin/sftp/dttest_cn/DsCenter_1687/krb5.conf AS krb5.conf;
 
 ADD FILE WITH /home/admin/sftp/dttest_cn/DsCenter_1687/krb5.conf RENAME test.conf;
@@ -10,8 +12,14 @@ ADD PYTHON_FILES WITH /test.py RENAME test.py;
 
 ADD PYTHON_REQUIREMENTS WITH /requirements.txt RENAME requirements.txt;
 
+ADD PYTHON_DEPENDENCIES WITH /dependencies.txt RENAME dependencies.txt;
+
 ADD PYTHON_JAR WITH /python_jar.jar RENAME py_jar.jar;
 
 ADD PYTHON_PARAMETER /parameter.py;
 
+ADD PYTHON_ARCHIVES WITH /archives.py RENAME archive.tx;
+
 ADD ENGINE FILE WITH /filePath RENAME theName KEY theKey;
+
+ADD CONFIG FILE WITH /config.ini FOR hiveConf as hadoop.cong.dir;
