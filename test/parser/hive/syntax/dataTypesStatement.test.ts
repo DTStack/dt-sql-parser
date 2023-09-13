a33b62a32be2f0167583e031773b4e1dff5d85e2
zhaoge <>
2023-09-13 15:04:53 +0800

    test: hive complete data types' check and unit tests

---
+++ test/parser/hive/syntax/dataTypesStatement.test.ts
@@ -0,0 +1,16 @@
+import HiveSQL from '../../../../src/parser/hive';
+import { readSQL } from '../../../helper';
+
+const parser = new HiveSQL();
+
+const features = {
+    dataTypes: readSQL(__dirname, 'dataTypes.sql'),
+};
+
+describe('HiveSQL Check Data Types Tests', () => {
+    features.dataTypes.forEach((dataType) => {
+        it(dataType, () => {
+            expect(parser.validate(dataType).length).toBe(0);
+        });
+    });
+});
