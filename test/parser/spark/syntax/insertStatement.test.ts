7bbaba639891be4f194a6df33de692f0adb27f01
琉易 <liuxy0551@qq.com>
2023-10-09 09:51:16 +0800

    test: spark DML test (#169)
    Co-authored-by: liuyi <liuyi@dtstack.com>

---
+++ test/parser/spark/syntax/insertStatement.test.ts
@@ -0,0 +1,22 @@
+import SparkSQL from '../../../../src/parser/spark';
+import { readSQL } from '../../../helper';
+
+
+const parser = new SparkSQL();
+
+const features = {
+    insertIntoTable: readSQL(__dirname, 'insertIntoTable.sql'),
+    insertOverwriteTable: readSQL(__dirname, 'insertOverwriteTable.sql'),
+    insertOverwriteDirectory: readSQL(__dirname, 'insertOverwriteDirectory.sql'),
+    loadData: readSQL(__dirname, 'loadData.sql'),
+};
+
+describe('SparkSQL Insert Syntax Tests', () => {
+    Object.keys(features).forEach((key) => {
+        features[key].forEach((sql) => {
+            it(sql, () => {
+                expect(parser.validate(sql).length).toBe(0);
+            });
+        });
+    });
+});
