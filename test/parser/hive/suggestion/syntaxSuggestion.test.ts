c4030929b2ae8b2b9e113e573c1c678f7cfe466e
Hayden <hayden9653@gmail.com>
2023-10-10 16:37:49 +0800

    Feat/auto complete (#175)
    * feat: update hive grammar to adapt to c3
    
    * feat: support viewName, dbName, fnName autoComplete to hive
    
    * test: add hive suggestion unit test
    
    * test: optimze flink suggestion unit tests

--- test/parser/hive/suggestion/syntaxSuggestion.test.ts
+++ test/parser/hive/suggestion/syntaxSuggestion.test.ts
@@ -1,109 +1,147 @@
 import fs from 'fs';
 import path from 'path';
 import { CaretPosition, SyntaxContextType } from '../../../../src/parser/common/basic-parser-types';
-import FlinkSQL from '../../../../src/parser/flinksql'
+import HiveSQL from '../../../../src/parser/hive'
 
 const syntaxSql = fs.readFileSync(path.join(__dirname, 'fixtures', 'syntaxSuggestion.sql'), 'utf-8');
-const multipleSql = fs.readFileSync(path.join(__dirname, 'fixtures', 'multipleSql.sql'), 'utf-8');
 
-describe('Flink SQL Syntax Suggestion', () => {
-    const parser = new FlinkSQL();
+describe('Hive SQL Syntax Suggestion', () => {
+    const parser = new HiveSQL();
 
     test('Validate Syntax SQL', () => {
         expect(parser.validate(syntaxSql).length).not.toBe(0);
         expect(parser.validate(syntaxSql).length).not.toBe(0);
         expect(parser.validate(syntaxSql).length).not.toBe(0);
-    })
+    });
 
     test('Insert table ', () => {
         const pos: CaretPosition = {
             lineNumber: 1,
-            column: 22
+            column: 18
         }
         const syntaxes = parser.getSuggestionAtCaretPosition(syntaxSql, pos)?.syntax;
         const suggestion = syntaxes?.find(syn => syn.syntaxContextType === SyntaxContextType.TABLE);
 
         expect(suggestion).not.toBeUndefined();
         expect(suggestion?.wordRanges.map(token => token.text))
-            .toEqual([ 'cat', '.', 'db', '.', 'tb' ])
-    })
+            .toEqual([ 'db', '.', 'tb' ])
+    });
 
-    test('Select table', () => {
+    test('Select table ', () => {
         const pos: CaretPosition = {
             lineNumber: 3,
-            column: 21
+            column: 18
         }
         const syntaxes = parser.getSuggestionAtCaretPosition(syntaxSql, pos)?.syntax;
         const suggestion = syntaxes?.find(syn => syn.syntaxContextType === SyntaxContextType.TABLE);
 
         expect(suggestion).not.toBeUndefined();
         expect(suggestion?.wordRanges.map(token => token.text))
-            .toEqual([ 'cat', '.', 'db' ])
-    })
+            .toEqual([ 'db', '.' ])
+    });
 
-    test('Create table', () => {
+    test('Create table ', () => {
         const pos: CaretPosition = {
             lineNumber: 5,
-            column: 20
+            column: 17
         }
         const syntaxes = parser.getSuggestionAtCaretPosition(syntaxSql, pos)?.syntax;
         const suggestion = syntaxes?.find(syn => syn.syntaxContextType === SyntaxContextType.TABLE_CREATE);
 
         expect(suggestion).not.toBeUndefined();
         expect(suggestion?.wordRanges.map(token => token.text))
-            .toEqual([ 'cat', '.', 'db' ])
-    })
+            .toEqual([ 'db', '.' ])
+    });
 
-    test('Show tables from', () => {
+    test('DROP table ', () => {
         const pos: CaretPosition = {
             lineNumber: 7,
-            column: 21
+            column: 26
         }
         const syntaxes = parser.getSuggestionAtCaretPosition(syntaxSql, pos)?.syntax;
         const suggestion = syntaxes?.find(syn => syn.syntaxContextType === SyntaxContextType.TABLE);
 
         expect(suggestion).not.toBeUndefined();
         expect(suggestion?.wordRanges.map(token => token.text))
-            .toEqual([ 'cat' ])
-    })
+            .toEqual([ 'db', '.', 'a' ])
+    });
 
-    test('Alter database', () => {
+    test('Create view ', () => {
         const pos: CaretPosition = {
             lineNumber: 9,
-            column: 20
+            column: 28
         }
         const syntaxes = parser.getSuggestionAtCaretPosition(syntaxSql, pos)?.syntax;
-        const suggestion = syntaxes?.find(syn => syn.syntaxContextType === SyntaxContextType.DATABASE);
+        const suggestion = syntaxes?.find(syn => syn.syntaxContextType === SyntaxContextType.VIEW_CREATE);
 
         expect(suggestion).not.toBeUndefined();
         expect(suggestion?.wordRanges.map(token => token.text))
-            .toEqual([ 'cat', '.' ])
-    })
+            .toEqual([ 'db', '.', 'v' ])
+    });
 
-    test('Use database', () => {
+    test('Drop view ', () => {
         const pos: CaretPosition = {
-            lineNumber: 9,
-            column: 20
+            lineNumber: 11,
+            column: 15
         }
         const syntaxes = parser.getSuggestionAtCaretPosition(syntaxSql, pos)?.syntax;
-        const suggestion = syntaxes?.find(syn => syn.syntaxContextType === SyntaxContextType.DATABASE);
+        const suggestion = syntaxes?.find(syn => syn.syntaxContextType === SyntaxContextType.VIEW);
 
         expect(suggestion).not.toBeUndefined();
         expect(suggestion?.wordRanges.map(token => token.text))
-            .toEqual([ 'cat', '.' ]);
-    })
+            .toEqual([ 'db', '.', 'v' ])
+    });
 
-    test("Multiple SQL use database", () => {
+    test('Create function ', () => {
+        const pos: CaretPosition = {
+            lineNumber: 13,
+            column: 20
+        }
+        const syntaxes = parser.getSuggestionAtCaretPosition(syntaxSql, pos)?.syntax;
+        const suggestion = syntaxes?.find(syn => syn.syntaxContextType === SyntaxContextType.FUNCTION_CREATE);
+
+        expect(suggestion).not.toBeUndefined();
+        expect(suggestion?.wordRanges.map(token => token.text))
+            .toEqual([ 'fn1' ])
+    });
+
+    test('Use function', () => {
+        const pos: CaretPosition = {
+            lineNumber: 15,
+            column: 27
+        }
+        const syntaxes = parser.getSuggestionAtCaretPosition(syntaxSql, pos)?.syntax;
+        const suggestion = syntaxes?.find(syn => syn.syntaxContextType === SyntaxContextType.FUNCTION);
+
+        expect(suggestion).not.toBeUndefined();
+        expect(suggestion?.wordRanges.map(token => token.text))
+            .toEqual([ 'calculate_age' ])
+    });
+
+    test('Create database', () => {
+        const pos: CaretPosition = {
+            lineNumber: 17,
+            column: 19
+        }
+        const syntaxes = parser.getSuggestionAtCaretPosition(syntaxSql, pos)?.syntax;
+        const suggestion = syntaxes?.find(syn => syn.syntaxContextType === SyntaxContextType.DATABASE_CREATE);
+
+        expect(suggestion).not.toBeUndefined();
+        expect(suggestion?.wordRanges.map(token => token.text))
+            .toEqual([ 'db' ])
+    });
+
+    test('Drop database', () => {
         const pos: CaretPosition = {
             lineNumber: 19,
-            column: 10,
+            column: 26
         }
-        const syntaxes = parser.getSuggestionAtCaretPosition(multipleSql, pos)?.syntax;
+        const syntaxes = parser.getSuggestionAtCaretPosition(syntaxSql, pos)?.syntax;
         const suggestion = syntaxes?.find(syn => syn.syntaxContextType === SyntaxContextType.DATABASE);
 
         expect(suggestion).not.toBeUndefined();
         expect(suggestion?.wordRanges.map(token => token.text))
-            .toEqual([ 'cat1', '.' ]);
-    })
+            .toEqual([ 'sch' ])
+    });
 
 })
