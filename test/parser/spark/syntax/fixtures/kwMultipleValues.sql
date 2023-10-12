3320f29c7d900e186e9f060a533d9d6ee871b3b2
liuyi <liuyi@dtstack.com>
2023-10-12 16:55:14 +0800

    test: KW_NOT KW_RLIKE split into two value

---
+++ test/parser/spark/syntax/fixtures/kwMultipleValues.sql
@@ -0,0 +1,6 @@
+SELECT * FROM table_name WHERE NOT (age > 30);
+SELECT * FROM table_name WHERE ! (age > 30);
+
+
+SELECT * FROM table_name WHERE name RLIKE 'M+';
+SELECT * FROM table_name WHERE name REGEXP 'M+';
