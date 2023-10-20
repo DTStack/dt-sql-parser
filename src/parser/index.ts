e805833bf07dee33a90544c9d352210f9531942a
hayden <hayden9653@gmail.com>
2023-10-20 16:00:12 +0800

    feat: export trino sql

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -4,3 +4,4 @@ export { default as HiveSQL } from './hive';
 export { default as FlinkSQL } from './flinksql';
 export { default as SparkSQL } from './spark';
 export { default as PostgresSQL } from './pgsql';
+export { default as TrinoSQL } from './trinosql';
