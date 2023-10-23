4dbf52bdd2f22ca0e127231727fc337ef7cf1753
hayden <hayden9653@gmail.com>
2023-10-23 20:38:42 +0800

    feat: trinosqlParser supports codeCompletion and spliting

--- src/parser/trinosql.ts
+++ src/parser/trinosql.ts
@@ -1,9 +1,10 @@
 import { Token } from 'antlr4ts';
 import { CandidatesCollection } from 'antlr4-c3';
 import { TrinoSqlLexer } from '../lib/trinosql/TrinoSqlLexer';
-import { TrinoSqlParser, ProgramContext } from '../lib/trinosql/TrinoSqlParser';
+import { TrinoSqlParser, ProgramContext, StatementContext } from '../lib/trinosql/TrinoSqlParser';
+import { TrinoSqlListener } from '../lib/trinosql/TrinoSqlListener';
 import BasicParser from './common/basicParser';
-import { Suggestions } from './common/basic-parser-types';
+import { Suggestions, SyntaxContextType, SyntaxSuggestion } from './common/basic-parser-types';
 
 export default class TrinoSQL extends BasicParser<TrinoSqlLexer, ProgramContext, TrinoSqlParser> {
     protected createLexerFormCharStream(charStreams) {
@@ -17,19 +18,110 @@ export default class TrinoSQL extends BasicParser<TrinoSqlLexer, ProgramContext,
     }
 
     protected get splitListener() {
-        return null as any;
+        return new TrinoSqlSplitListener();
     }
 
-    protected preferredRules: Set<number> = new Set();
+    protected preferredRules: Set<number> = new Set([
+        TrinoSqlParser.RULE_catalogName,
+        TrinoSqlParser.RULE_catalogNameCreate,
+        TrinoSqlParser.RULE_schemaName,
+        TrinoSqlParser.RULE_schemaNameCreate,
+        TrinoSqlParser.RULE_tableName,
+        TrinoSqlParser.RULE_tableNameCreate,
+        TrinoSqlParser.RULE_viewName,
+        TrinoSqlParser.RULE_viewNameCreate,
+        TrinoSqlParser.RULE_functionName,
+    ]);
 
     protected processCandidates(
         candidates: CandidatesCollection,
         allTokens: Token[],
-        caretTokenIndex: number
+        caretTokenIndex: number,
+        tokenIndexOffset: number
     ): Suggestions<Token> {
+        const originalSyntaxSuggestions: SyntaxSuggestion<Token>[] = [];
+        const keywords: string[] = [];
+
+        for (let candidate of candidates.rules) {
+            const [ruleType, candidateRule] = candidate;
+            const startTokenIndex = candidateRule.startTokenIndex + tokenIndexOffset;
+            const tokenRanges = allTokens.slice(
+                startTokenIndex,
+                caretTokenIndex + tokenIndexOffset + 1
+            );
+
+            let syntaxContextType: SyntaxContextType;
+            switch (ruleType) {
+                case TrinoSqlParser.RULE_catalogName: {
+                    syntaxContextType = SyntaxContextType.CATALOG;
+                    break;
+                }
+                case TrinoSqlParser.RULE_schemaName: {
+                    syntaxContextType = SyntaxContextType.DATABASE;
+                    break;
+                }
+                case TrinoSqlParser.RULE_schemaNameCreate: {
+                    syntaxContextType = SyntaxContextType.DATABASE_CREATE;
+                    break;
+                }
+                case TrinoSqlParser.RULE_tableName: {
+                    syntaxContextType = SyntaxContextType.TABLE;
+                    break;
+                }
+                case TrinoSqlParser.RULE_tableNameCreate: {
+                    syntaxContextType = SyntaxContextType.TABLE_CREATE;
+                    break;
+                }
+                case TrinoSqlParser.RULE_viewName: {
+                    syntaxContextType = SyntaxContextType.VIEW;
+                    break;
+                }
+                case TrinoSqlParser.RULE_viewNameCreate: {
+                    syntaxContextType = SyntaxContextType.VIEW_CREATE;
+                    break;
+                }
+                case TrinoSqlParser.RULE_functionName: {
+                    syntaxContextType = SyntaxContextType.FUNCTION;
+                    break;
+                }
+                default:
+                    break;
+            }
+
+            if (syntaxContextType) {
+                originalSyntaxSuggestions.push({
+                    syntaxContextType,
+                    wordRanges: tokenRanges,
+                });
+            }
+        }
+
+        for (let candidate of candidates.tokens) {
+            const symbolicName = this._parser.vocabulary.getSymbolicName(candidate[0]);
+            const displayName = this._parser.vocabulary.getDisplayName(candidate[0]);
+            if (symbolicName && symbolicName.startsWith('KW_')) {
+                const keyword =
+                    displayName.startsWith("'") && displayName.endsWith("'")
+                        ? displayName.slice(1, -1)
+                        : displayName;
+                keywords.push(keyword);
+            }
+        }
         return {
-            syntax: [],
-            keywords: [],
+            syntax: originalSyntaxSuggestions,
+            keywords,
         };
+    }
+}
+
+export class TrinoSqlSplitListener implements TrinoSqlListener {
+    private _statementsContext: StatementContext[] = [];
+
+    exitStatement = (ctx: StatementContext) => {
+        this._statementsContext.push(ctx);
+    };
+
+    get statementsContext() {
+        return this._statementsContext;
     }
 }
