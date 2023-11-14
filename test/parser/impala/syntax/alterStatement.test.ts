8e4933f7bfba217f8b601be8cb0521e8b925985d
LuckyFBB <976060700@qq.com>
2023-11-14 13:50:46 +0800

    feat(impala): add alter db sql

--- test/parser/impala/syntax/alterStatement.test.ts
+++ test/parser/impala/syntax/alterStatement.test.ts
@@ -5,11 +5,19 @@ const parser = new ImpalaSQL();
 
 const features = {
     tables: readSQL(__dirname, 'alter_table.sql'),
+    dbs: readSQL(__dirname, 'alter_table.sql'),
 };
 
 describe('ImpalaSQL Alter Syntax Tests', () => {
     describe('ALTER TABLE', () => {
         features.tables.forEach((db) => {
+            it(db, () => {
+                expect(parser.validate(db).length).toBe(0);
+            });
+        });
+    });
+    describe('ALTER DATABASE', () => {
+        features.dbs.forEach((db) => {
             it(db, () => {
                 expect(parser.validate(db).length).toBe(0);
             });
