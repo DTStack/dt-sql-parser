0564db6a6a76437ea3416b28a8ae5ac7ff2ddd6c
hayden <hayden9653@gmail.com>
2023-11-23 11:31:23 +0800

    feat: hive column suggestion unit test

--- test/parser/hive/suggestion/fixtures/syntaxSuggestion.sql
+++ test/parser/hive/suggestion/fixtures/syntaxSuggestion.sql
@@ -17,3 +17,19 @@ SELECT name, calculate_age(birthday) AS age FROM students;
 CREATE DATABASE db;
 
 DROP SCHEMA IF EXISTS sch;
+
+SELECT co;
+
+SELECT  ;
+
+INSERT INTO tb ( );
+
+SELECT * FROM tb1 ORDER BY tb.c;
+
+CREATE TABLE newtb ( id INT, n );
+
+MERGE INTO tablename USING tablename2 ON (tablename.id = tablename2.id) WHEN MATCHED THEN UPDATE SET tablename.col ;
+
+ALTER TABLE tbl CHANGE COLUMN  ;
+
+ALTER TABLE tbl CHANGE COLUMN tbl.oldcol new ;
