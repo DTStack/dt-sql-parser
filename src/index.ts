762363d687418844532dd357bda72e4afe8f45bd
hayden <hayden9653@gmail.com>
2023-12-19 17:22:14 +0800

    fix: #236 export AbstractParseTreeVisitor

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,45 @@
-export * from './parser';
-export * from './lib/flinksql/FlinkSqlParserListener';
-export * from './lib/flinksql/FlinkSqlParserVisitor';
-export * from './lib/mysql/MySqlParserVisitor';
-export * from './lib/mysql/MySqlParserListener';
-export * from './lib/hive/HiveSqlParserListener';
-export * from './lib/hive/HiveSqlParserVisitor';
-export * from './lib/plsql/PlSqlParserListener';
-export * from './lib/plsql/PlSqlParserVisitor';
-export * from './lib/spark/SparkSqlParserVisitor';
-export * from './lib/spark/SparkSqlParserListener';
-export * from './lib/pgsql/PostgreSQLParserListener';
-export * from './lib/pgsql/PostgreSQLParserVisitor';
-export * from './lib/trinosql/TrinoSqlListener';
-export * from './lib/trinosql/TrinoSqlVisitor';
-export * from './lib/impala/ImpalaSqlParserListener';
-export * from './lib/impala/ImpalaSqlParserVisitor';
+export { AbstractParseTreeVisitor } from 'antlr4ts/tree/AbstractParseTreeVisitor';
+
+export {
+    MySQL,
+    FlinkSQL,
+    SparkSQL,
+    HiveSQL,
+    PostgresSQL,
+    TrinoSQL,
+    ImpalaSQL,
+    PLSQL,
+} from './parser';
+
+export {
+    MySqlParserListener,
+    MySqlParserVisitor,
+    FlinkSqlParserListener,
+    FlinkSqlParserVisitor,
+    SparkSqlParserListener,
+    SparkSqlParserVisitor,
+    HiveSqlParserListener,
+    HiveSqlParserVisitor,
+    PlSqlParserListener,
+    PlSqlParserVisitor,
+    PostgreSQLParserListener,
+    PostgreSQLParserVisitor,
+    TrinoSqlListener,
+    TrinoSqlVisitor,
+    ImpalaSqlParserListener,
+    ImpalaSqlParserVisitor,
+} from './lib';
+
 export { SyntaxContextType } from './parser/common/basic-parser-types';
 
-export type * from './parser/common/basic-parser-types';
+export type {
+    CaretPosition,
+    WordRange,
+    Suggestions,
+    SyntaxSuggestion,
+    TextSlice,
+} from './parser/common/basic-parser-types';
+
 export type { SyntaxError, ParseError, ErrorListener } from './parser/common/parseErrorListener';
 
 /**
---
+++ src/lib/index.ts
@@ -0,0 +1,23 @@
+export { FlinkSqlParserListener } from './flinksql/FlinkSqlParserListener';
+export { FlinkSqlParserVisitor } from './flinksql/FlinkSqlParserVisitor';
+
+export { MySqlParserListener } from './mysql/MySqlParserListener';
+export { MySqlParserVisitor } from './mysql/MySqlParserVisitor';
+
+export { HiveSqlParserListener } from './hive/HiveSqlParserListener';
+export { HiveSqlParserVisitor } from './hive/HiveSqlParserVisitor';
+
+export { PlSqlParserListener } from './plsql/PlSqlParserListener';
+export { PlSqlParserVisitor } from './plsql/PlSqlParserVisitor';
+
+export { SparkSqlParserListener } from './spark/SparkSqlParserListener';
+export { SparkSqlParserVisitor } from './spark/SparkSqlParserVisitor';
+
+export { PostgreSQLParserListener } from './pgsql/PostgreSQLParserListener';
+export { PostgreSQLParserVisitor } from './pgsql/PostgreSQLParserVisitor';
+
+export { TrinoSqlListener } from './trinosql/TrinoSqlListener';
+export { TrinoSqlVisitor } from './trinosql/TrinoSqlVisitor';
+
+export { ImpalaSqlParserListener } from './impala/ImpalaSqlParserListener';
+export { ImpalaSqlParserVisitor } from './impala/ImpalaSqlParserVisitor';
