aafbf97f612f49e28eb415111f7fcd4d7ccbf2cc
hayden <hayden9653@gmail.com>
2024-01-23 18:16:33 +0800

    feat: add mysql entity collector

---
+++ src/parser/mysql/mysqlEntityCollector.ts
@@ -0,0 +1,213 @@
+import type {
+    ColumnCreateTableContext,
+    ColumnNameCreateContext,
+    CopyCreateTableContext,
+    CreateTableContext,
+    CreateViewContext,
+    DatabaseNameContext,
+    DatabaseNameCreateContext,
+    FunctionNameCreateContext,
+    InsertStatementContext,
+    QueryCreateTableContext,
+    SelectStatementContext,
+    SingleStatementContext,
+    TableNameContext,
+    TableNameCreateContext,
+    ViewNameContext,
+    ViewNameCreateContext,
+} from '../../lib/mysql/MySqlParser';
+import type { MySqlParserListener } from '../../lib/mysql/MySqlParserListener';
+import { EntityContextType } from '../common/basic-parser-types';
+import EntityCollector, {
+    EntityContext,
+    StmtContext,
+    StmtContextType,
+} from '../common/entityCollector';
+
+export default class MySqlEntityCollector extends EntityCollector implements MySqlParserListener {
+    combineRootStmtEntities(
+        stmtContext: StmtContext,
+        entitiesInsideStmt: EntityContext[]
+    ): EntityContext[] {
+        if (
+            stmtContext.stmtContextType === StmtContextType.CREATE_TABLE_STMT ||
+            stmtContext.stmtContextType === StmtContextType.CREATE_VIEW_STMT
+        ) {
+            return this.combineCreateTableOrViewStmtEntities(stmtContext, entitiesInsideStmt);
+        }
+        return entitiesInsideStmt;
+    }
+
+    combineCreateTableOrViewStmtEntities(
+        stmtContext: StmtContext,
+        entitiesInsideStmt: EntityContext[]
+    ): EntityContext[] {
+        debugger;
+        const columns: EntityContext[] = [];
+        const relatedEntities: EntityContext[] = [];
+        let mainEntity: EntityContext = null;
+        const finalEntities = entitiesInsideStmt.reduce((result, entity) => {
+            if (entity.belongStmt !== stmtContext) {
+                if (
+                    entity.entityContextType !== EntityContextType.COLUMN &&
+                    entity.entityContextType !== EntityContextType.COLUMN_CREATE
+                ) {
+                    relatedEntities.push(entity);
+                    result.push(entity);
+                }
+                return result;
+            }
+
+            if (entity.entityContextType === EntityContextType.COLUMN_CREATE) {
+                columns.push(entity);
+            } else if (
+                entity.entityContextType === EntityContextType.TABLE_CREATE ||
+                entity.entityContextType === EntityContextType.VIEW_CREATE
+            ) {
+                mainEntity = entity;
+                result.push(entity);
+                return result;
+            } else if (entity.entityContextType !== EntityContextType.COLUMN) {
+                relatedEntities.push(entity);
+                result.push(entity);
+            }
+            return result;
+        }, []);
+
+        if (columns.length) {
+            mainEntity.columns = columns;
+        }
+
+        if (relatedEntities.length) {
+            mainEntity.relatedEntities = relatedEntities;
+        }
+
+        return finalEntities;
+    }
+
+    /** ====== Entity Begin */
+    exitDatabaseName(ctx: DatabaseNameContext) {
+        this.pushEntity(ctx, EntityContextType.DATABASE);
+    }
+
+    exitDatabaseNameCreate(ctx: DatabaseNameCreateContext) {
+        this.pushEntity(ctx, EntityContextType.DATABASE_CREATE);
+    }
+
+    exitTableName(ctx: TableNameContext) {
+        this.pushEntity(ctx, EntityContextType.TABLE);
+    }
+
+    exitTableNameCreate(ctx: TableNameCreateContext) {
+        this.pushEntity(ctx, EntityContextType.TABLE_CREATE);
+    }
+
+    exitViewName(ctx: ViewNameContext) {
+        this.pushEntity(ctx, EntityContextType.VIEW);
+    }
+
+    exitViewNameCreate(ctx: ViewNameCreateContext) {
+        this.pushEntity(ctx, EntityContextType.VIEW_CREATE);
+    }
+
+    exitFunctionNameCreate(ctx: FunctionNameCreateContext) {
+        this.pushEntity(ctx, EntityContextType.FUNCTION_CREATE);
+    }
+
+    exitColumnNameCreate(ctx: ColumnNameCreateContext) {
+        this.pushEntity(ctx, EntityContextType.COLUMN_CREATE);
+    }
+
+    /** ===== Statement begin */
+    enterSingleStatement(ctx: SingleStatementContext) {
+        this.pushStmt(ctx, StmtContextType.COMMON_STMT);
+    }
+
+    exitSingleStatement(ctx: SingleStatementContext) {
+        this.popStmt();
+    }
+
+    enterCreateTable(ctx: CreateTableContext) {
+        this.pushStmt(ctx, StmtContextType.CREATE_TABLE_STMT);
+    }
+
+    enterQueryCreateTable(ctx: QueryCreateTableContext) {
+        this.pushStmt(ctx, StmtContextType.CREATE_TABLE_STMT);
+    }
+
+    exitQueryCreateTable(ctx: QueryCreateTableContext) {
+        this.popStmt();
+    }
+
+    enterColumnCreateTable(ctx: ColumnCreateTableContext) {
+        this.pushStmt(ctx, StmtContextType.CREATE_TABLE_STMT);
+    }
+
+    exitColumnCreateTable(ctx: ColumnCreateTableContext) {
+        this.popStmt();
+    }
+
+    enterCopyCreateTable(ctx: CopyCreateTableContext) {
+        this.pushStmt(ctx, StmtContextType.CREATE_TABLE_STMT);
+    }
+
+    exitCopyCreateTable(ctx: CopyCreateTableContext) {
+        this.popStmt();
+    }
+
+    enterCreateView(ctx: CreateViewContext) {
+        this.pushStmt(ctx, StmtContextType.CREATE_VIEW_STMT);
+    }
+
+    exitCreateView(ctx: CreateViewContext) {
+        this.popStmt();
+    }
+
+    enterSimpleSelect(ctx: SelectStatementContext) {
+        this.pushStmt(ctx, StmtContextType.SELECT_STMT);
+    }
+
+    exitSimpleSelect(ctx: SelectStatementContext) {
+        this.popStmt();
+    }
+
+    enterParenthesisSelect(ctx: SelectStatementContext) {
+        this.pushStmt(ctx, StmtContextType.SELECT_STMT);
+    }
+
+    exitParenthesisSelect(ctx: SelectStatementContext) {
+        this.popStmt();
+    }
+
+    enterUnionSelect(ctx: SelectStatementContext) {
+        this.pushStmt(ctx, StmtContextType.SELECT_STMT);
+    }
+
+    exitUnionSelect(ctx: SelectStatementContext) {
+        this.popStmt();
+    }
+
+    enterParenthesisUnionSelect(ctx: SelectStatementContext) {
+        this.pushStmt(ctx, StmtContextType.SELECT_STMT);
+    }
+
+    exitParenthesisUnionSelect(ctx: SelectStatementContext) {
+        this.popStmt();
+    }
+
+    enterWithLateralStatement(ctx: SelectStatementContext) {
+        this.pushStmt(ctx, StmtContextType.SELECT_STMT);
+    }
+
+    exitWithLateralStatement(ctx: SelectStatementContext) {
+        this.popStmt();
+    }
+
+    enterInsertStatement(ctx: InsertStatementContext) {
+        this.pushStmt(ctx, StmtContextType.INSERT_STMT);
+    }
+
+    exitInsertStatement(ctx: InsertStatementContext) {
+        this.popStmt();
+    }
+}
