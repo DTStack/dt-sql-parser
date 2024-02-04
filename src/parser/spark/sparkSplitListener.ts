a3dee2677474a5d60d2e8fe3154922a0457fe5e5
hayden <hayden9653@gmail.com>
2024-02-04 16:29:14 +0800

    feat: add spark entity collector

---
+++ src/parser/spark/sparkSplitListener.ts
@@ -0,0 +1,17 @@
+import { SingleStatementContext } from '../../lib/spark/SparkSqlParser';
+
+import { SparkSqlParserListener } from '../../lib/spark/SparkSqlParserListener';
+
+export default class SparkSqlSplitListener implements SparkSqlParserListener {
+    private _statementsContext: SingleStatementContext[] = [];
+
+    exitSingleStatement = (ctx: SingleStatementContext) => {
+        this._statementsContext.push(ctx);
+    };
+
+    enterSingleStatement = (ctx: SingleStatementContext) => {};
+
+    get statementsContext() {
+        return this._statementsContext;
+    }
+}
