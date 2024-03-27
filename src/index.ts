3a78280f30321853af4deb217f237d85ad6fe136
Hayden <hayden9653@gmail.com>
2024-03-27 16:49:47 +0800

    feat: export StmtContextType as enum

--- src/index.ts
+++ src/index.ts
@@ -38,13 +38,15 @@ export {
     EntityContextType as SyntaxContextType,
 } from './parser/common/types';
 
+export { StmtContextType } from './parser/common/entityCollector';
+
 export type { CaretPosition, Suggestions, SyntaxSuggestion } from './parser/common/types';
 
 export type { WordRange, TextSlice } from './parser/common/textAndWord';
 
 export type { SyntaxError, ParseError, ErrorListener } from './parser/common/parseErrorListener';
 
-export type { StmtContextType, StmtContext, EntityContext } from './parser/common/entityCollector';
+export type { StmtContext, EntityContext } from './parser/common/entityCollector';
 
 /**
  * @deprecated Legacy utils will be removed when the stable version is released.
