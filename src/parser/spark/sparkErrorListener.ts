fed0b5b7ef620bbdce3f7439ebdf329a84c3ec47
LuckyFBB <976060700@qq.com>
2024-10-16 13:53:36 +0800

    feat(spark): add spark expression column

--- src/parser/spark/sparkErrorListener.ts
+++ src/parser/spark/sparkErrorListener.ts
@@ -17,6 +17,7 @@ export class SparkErrorListener extends ParseErrorListener {
         [SparkSqlParser.RULE_functionName, 'function'],
         [SparkSqlParser.RULE_functionNameCreate, 'function'],
         [SparkSqlParser.RULE_columnName, 'column'],
+        [SparkSqlParser.RULE_columnNamePath, 'column'],
         [SparkSqlParser.RULE_columnNameCreate, 'column'],
     ]);
 
@@ -48,7 +49,8 @@ export class SparkErrorListener extends ParseErrorListener {
                     case SparkSqlParser.RULE_tableName:
                     case SparkSqlParser.RULE_viewName:
                     case SparkSqlParser.RULE_functionName:
-                    case SparkSqlParser.RULE_columnName: {
+                    case SparkSqlParser.RULE_columnName:
+                    case SparkSqlParser.RULE_columnNamePath: {
                         result.push(`{existing}${name}`);
                         break;
                     }
